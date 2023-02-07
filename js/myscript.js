4952f10798a23aaa9e4cd4477479bef381e2bd08
Rocca-Francesco <roccafrance98@yahoo.it>
2023-02-07 18:16:01 +0100

    image-big-change

--- js/myscript.js
+++ js/myscript.js
@@ -1,24 +1,82 @@
 // creo il mio array con i contenuti per il carosello
 const images = [
     {
-        image: 'img/01.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
     }, {
-        image: 'img/02.webp',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
     }, {
-        image: 'img/03.webp',
         title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
     }, {
-        image: 'img/04.webp',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
     }, {
-        image: 'img/05.webp',
         title: 'Marvel\'s Avengers',
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+// prendo il mio carosello
+const corouselEl = document.getElementById("carouselBig");
+
+// prendo la mia freccia NEXT
+const nextEl = document.getElementById("arrow-next");
+// dichiaro una variabile per capire in che posizione è il mio carosello
+let currentPosition = 0;
+// dichiaro una variabile per capire se sono nel range del carosello
+const maxPosition = 4;
+
+nextEl.addEventListener(
+    "click",
+    function () {
+        // aggiungo il click
+        currentPosition++;
+        // controllo di essere nel range del carosello
+        if (currentPosition > maxPosition) {
+            currentPosition = 0;
+        }
+        // chiamo la funzione per decidere cosa mostrare
+        positionCarouselToDecide(currentPosition);
+        console.log(currentPosition);
+    }
+)
+
+// prendo la mia freccia NEXT
+const prevEl = document.getElementById("arrow-prev");
+
+prevEl.addEventListener(
+    "click",
+    function () {
+        // rimuovo il click
+        currentPosition--;
+        // controllo di essere nel range del carosello
+        if (currentPosition < 0) {
+            currentPosition = maxPosition;
+        }
+        // chiamo la funzione per decidere cosa mostrare
+        positionCarouselToDecide(currentPosition);
+        console.log(currentPosition);
+    }
+)
+
+function positionCarouselToDecide(position) {
+    // scelgo l'immagine di sfondo
+    if (position == 0) {
+        corouselEl.classList.remove("backgroundFIVE");
+        corouselEl.classList.add("backgroundONE");
+    } else if (position == 1) {
+        corouselEl.classList.remove("backgroundONE");
+        corouselEl.classList.add("backgroundTWO");
+    } else if (position == 2) {
+        corouselEl.classList.remove("backgroundTWO");
+        corouselEl.classList.add("backgroundTHREE");
+    } else if (position == 3) {
+        corouselEl.classList.remove("backgroundTHREE");
+        corouselEl.classList.add("backgroundFOUR");
+    } else if (position == 4) {
+        corouselEl.classList.remove("backgroundFOUR");
+        corouselEl.classList.add("backgroundFIVE");
+    }
+}
